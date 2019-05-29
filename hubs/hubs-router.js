b0f5cd138387b19c664d15bb87cffe00de5221db
Seth Lachman <lachmanseth17@gmail.com>
2019-05-29 15:10:10 -0400

    Initial commit.

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 
-const Hubs = require('./hubs-model.js');
-const Messages = require('../messages/messages-model.js');
+const Hubs = require("./hubs-model.js");
+const Messages = require("../messages/messages-model.js");
 
 const router = express.Router();
 
 // this only runs if the url has /api/hubs in it
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const hubs = await Hubs.find(req.query);
     res.status(200).json(hubs);
@@ -14,81 +14,90 @@ router.get('/', async (req, res) => {
     // log error to server
     console.log(error);
     res.status(500).json({
-      message: 'Error retrieving the hubs',
+      message: "Error retrieving the hubs"
     });
   }
 });
 
 // /api/hubs/:id
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const hub = await Hubs.findById(req.params.id);
 
     if (hub) {
       res.status(200).json(hub);
     } else {
-      res.status(404).json({ message: 'Hub not found' });
+      res.status(404).json({ message: "Hub not found" });
     }
   } catch (error) {
     // log error to server
     console.log(error);
     res.status(500).json({
-      message: 'Error retrieving the hub',
+      message: "Error retrieving the hub"
     });
   }
 });
 
-router.post('/', async (req, res) => {
+function validateBody(req, res, next) {
+  if (req.body && req.body.name) {
+    next();
+  } else {
+    res.status(400).json({ message: "Please provide the name of the hub" });
+  }
+}
+
+router.post("/", async (req, res) => {
   try {
+    // validate body to make sure there is a name
     const hub = await Hubs.add(req.body);
     res.status(201).json(hub);
   } catch (error) {
     // log error to server
     console.log(error);
     res.status(500).json({
-      message: 'Error adding the hub',
+      message: "Error adding the hub"
     });
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const count = await Hubs.remove(req.params.id);
     if (count > 0) {
-      res.status(200).json({ message: 'The hub has been nuked' });
+      res.status(200).json({ message: "The hub has been nuked" });
     } else {
-      res.status(404).json({ message: 'The hub could not be found' });
+      res.status(404).json({ message: "The hub could not be found" });
     }
   } catch (error) {
     // log error to server
     console.log(error);
     res.status(500).json({
-      message: 'Error removing the hub',
+      message: "Error removing the hub"
     });
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const hub = await Hubs.update(req.params.id, req.body);
     if (hub) {
       res.status(200).json(hub);
     } else {
-      res.status(404).json({ message: 'The hub could not be found' });
+      res.status(404).json({ message: "The hub could not be found" });
     }
   } catch (error) {
     // log error to server
     console.log(error);
     res.status(500).json({
-      message: 'Error updating the hub',
+      message: "Error updating the hub"
     });
   }
 });
 
 // add an endpoint that returns all the messages for a hub
 // this is a sub-route or sub-resource
-router.get('/:id/messages', async (req, res) => {
+router.get("/:id/messages", async (req, res) => {
   try {
     const messages = await Hubs.findHubMessages(req.params.id);
 
@@ -97,13 +106,13 @@ router.get('/:id/messages', async (req, res) => {
     // log error to server
     console.log(error);
     res.status(500).json({
-      message: 'Error getting the messages for the hub',
+      message: "Error getting the messages for the hub"
     });
   }
 });
 
 // add an endpoint for adding new message to a hub
-router.post('/:id/messages', async (req, res) => {
+router.post("/:id/messages", async (req, res) => {
   const messageInfo = { ...req.body, hub_id: req.params.id };
 
   try {
@@ -113,7 +122,7 @@ router.post('/:id/messages', async (req, res) => {
     // log error to server
     console.log(error);
     res.status(500).json({
-      message: 'Error getting the messages for the hub',
+      message: "Error getting the messages for the hub"
     });
   }
 });
--- server.js
+++ server.js
@@ -1,20 +1,39 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
-server.use(express.json());
+// global middleware
+server.use(express.json()); // built-in middleware provided by express
+server.use(helmet());
+server.use(logger);
 
-server.use('/api/hubs', hubsRouter);
+server.use("/api/hubs", hubsRouter);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.get("/", addName, (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+function logger(req, res, next) {
+  console.log(`A ${req.method} request to '${req.url}'`);
+  next();
+}
+
+function addName(req, res, next) {
+  console.log("\nname added\n");
+  req.name = "Web XIX";
+  next();
+}
+
+function stopRequest(req, res, next) {
+  res.status(401).json({ message: "you shall not pass!" });
+}
 
 module.exports = server;
